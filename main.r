6289bbd2a7f74df31537a0a62b8855adfe2af75c
5deen <19186497-5deen@users.noreply.replit.com>
2023-11-14 21:28:18 +0000

    Simple matrix function added

--- main.r
+++ main.r
@@ -1,1 +1,7 @@
-print("hello world")
+# Write a function with a matrix
+simple_matrix <- function() {
+  mymatrix <- matrix(c(1,2,3,4,5,6), nrow = 3, ncol = 2)
+  # Print the matrix
+  mymatrix 
+}
+simple_matrix()
